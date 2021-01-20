9215fac4a4451b2bb268139eed69c88e222343f7
Peyo83 <jpvase@gmail.com>
2021-01-20 01:04:34 +0100

    Constructores en Java y JS

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("AW3423", new Account("Manolo", "ERCAWD"))
+car.passenger = 4;
+car.printDataCar();
--- Java/Main.java
+++ Java/Main.java
@@ -2,16 +2,11 @@ package Java;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
         car.passenger = 4;
         car.printDataCar();
-        
-        Car car2 = new Car();
-        car2.license = "MMA223";
-        car2.driver = "Andrea Herrera";
+
+        Car car2 = new Car("NAQ432", new Account("Manolo", "PLK987"));
         car2.passenger = 2;
         car2.printDataCar();
     }
